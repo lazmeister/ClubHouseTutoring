7e26912609765d48e8ac7736178ed48897a2c6fc
Laszlo Alt <laszlo.alt@gmail.com>
2023-02-16 19:05:04 -0500

    first commit

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -9,26 +9,27 @@ const HeroSection = () => {
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
-          <Image
-            src="/headshot.png"
-            alt=""
-            width={325}
-            height={325}
-            className="rounded-full shadow-2xl"
-          />
+            <Image
+              src="/home-1.jpg"
+              alt=""
+              width={425}
+              height={425}
+            />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Hosna!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-4xl">Honestly, there&#39;s no such thing as an educational emergency!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&#39;m a{" "}
-            <span className="font-semibold text-teal-600">
-              Software Engineer{" "}
-            </span>
-            based in Los Angeles, CA. Working towards creating software that
-            makes life easier and more meaningful.
+          As parents, we know how hard it is to watch your
+          child struggle as you search for ways to help.
+          If you&#39;ve been there too, please know you&#39;ve made
+          it to the right place. So, take a deep breath,
+          relax those shoulders, and let us show you just
+          how quickly we can turn things around. 
+
           </p>
+            <br />
           <Link
-            to="projects"
+            to="method"
             className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
             activeClass="active"
             spy={true}
@@ -36,7 +37,7 @@ const HeroSection = () => {
             offset={-100}
             duration={500}
           >
-            Projects
+            Learn More
           </Link>
         </div>
       </div>
--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -32,9 +32,9 @@ const projects = [
 
 const ProjectsSection = () => {
   return (
-    <section id="projects">
+    <section id="method">
       <h1 className="my-10 text-center font-bold text-4xl">
-        Projects
+        Method
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
       </h1>
 
